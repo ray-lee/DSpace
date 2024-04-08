e4d785765272915038e7270d839228d9ca3d70e3
Ray Lee <ray.lee@lyrasis.org>
2024-04-08 16:42:12 -0400

    Add SAML login filter.

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/security/WebSecurityConfiguration.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/security/WebSecurityConfiguration.java
@@ -158,6 +158,11 @@ public class WebSecurityConfiguration {
             .addFilterBefore(new OidcLoginFilter("/api/authn/oidc", authenticationManager,
                                                       restAuthenticationService),
                              LogoutFilter.class)
+            //Add a filter before our SAML endpoints to do the authentication based on the data in the
+            // HTTP request
+            .addFilterBefore(new SamlLoginFilter("/api/authn/saml", authenticationManager,
+                                                 restAuthenticationService),
+                             LogoutFilter.class)
             // Add a custom Token based authentication filter based on the token previously given to the client
             // before each URL
             .addFilterBefore(new StatelessAuthenticationFilter(authenticationManager, restAuthenticationService,
